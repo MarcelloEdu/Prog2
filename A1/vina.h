f723fcd49869282352c8206dac4b99967e897f69
MarcelloEdu <eduardomarcello912@gmail.com>
2025-04-25 16:04:17 -0300

    [UPDATE] adicionando alguns comentarios e renomeando remove() pois já existe uma função com esse nome na lib padrão

--- A1/vina.h
+++ A1/vina.h
@@ -13,16 +13,29 @@ typedef struct {
     int is_compressed;        // flag: 0 = plano, 1 = comprimido
 } EntradaVC;
 
+// Carrega o diretório do archive (.vc) para a memória (tabela de membros)
+int load_directory(FILE *archive, EntradaVC **table, int *num_members);
+
+// Salva o diretório atualizado no final do archive
+int save_directory(FILE *archive, EntradaVC **table, int *num_membros);
+
 //ponteiro duplo pois é uma lista de strings
 //const char para indicar que o nome do arquivo é apenas para leitura
+
+// Insere arquivos sem compressão no archive (.vc)
 int insert_plan(const char *archive_name, char **members, int num_members);
 
+// Insere arquivos comprimidos com LZ (usa plano se compressão não for efetiva)
 int insert_compressed(const char *archive_name, char **members, int num_members);
 
+// Move um membro para depois de outro (muda apenas a ordem lógica no diretório)
 void move_member(const char *archive_name, const char *target, const char *member_to_move);
 
+// Extrai os arquivos indicados (ou todos se members == NULL)
 int extract(const char *archive_name, char **members, int num_members);
 
-void remove(const char *archive_name, char **members, int num_members);
+// Remove os membros indicados do archive (ajusta diretório)
+void remove_members(const char *archive_name, char **members, int num_members);
 
+// Lista as informações dos membros do archive no terminal
 void list_content(const char *archive_name);
